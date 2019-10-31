c660ed6be6a898b8266633bc6ee8d066792ab30e
simonheglasik <simonheglasik@gmail.com>
2019-10-31 17:30:44 +0100

    03.07

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            return people * each <= total;
         }
     }
 }
